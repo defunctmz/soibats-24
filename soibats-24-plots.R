2e1b33901a0a92646035f7ea9ef3e862f5955a72
defunctmz <maazmohammed800@gmail.com>
2025-01-08 11:08:06 +0530

    EB plots stage 1

--- soibats-24-plots.R
+++ soibats-24-plots.R
@@ -1,13 +1,11 @@
 # Packages ----
 install.packages("pacman")
-pacman::p_load(pacman,tidyverse)
+pacman::p_load(pacman,tidyverse,lubridate)
 
 # EB group - Overall - Data cleaning ----
 
-# Paper publications distribution by year, region and categories
-
 # import datasheet
-eb_basedt <- read_csv("eb_citations_data.csv",
+eb_basedt <- read_csv("eb_citations_data_v2.csv",
                       skip = 1,
                       name_repair = "universal",
                       trim_ws = T)
@@ -18,27 +16,42 @@ View(eb_basedt)
 eb_basedt <- eb_basedt %>% 
   mutate(id = seq.int(1,nrow(eb_basedt),1))
 
-# long form categories----
+# long form categories ----
 eb_dt_cat <- eb_basedt %>%
   select(citation:Toxicology,id) %>% 
   pivot_longer(cols = Diet:Toxicology,
                names_to = "category",
                values_drop_na = T) 
 
-# long form states----
+eb_dt_cat[which(duplicated(eb_dt_cat$id)),]
+
+View(eb_dt_cat)
+# long form states (location) ----
 eb_dt_location <- eb_basedt %>% 
   select(citation,Andaman.and.Nicobar.Islands:West.Bengal,id) %>% 
   pivot_longer(cols = Andaman.and.Nicobar.Islands:West.Bengal,
                names_to = "location",
                values_drop_na = T)
 
-# check all citations are included - ignore this df
-test <- eb_dt_location %>% select(-location) %>% group_by(id,value) %>% unique()
+View(eb_dt_location)
 
-# Plowright et al 2019 missing (id = 87) because no location provided (can be assigned Kerala if necessary)
-# letting it be
+# check for duplicated entries
 
-# long form year of study----
+nrow(eb_dt_location)
+eb_dt_location[which(duplicated(eb_dt_location$id)),]
+nrow(eb_dt_location) - count(eb_dt_location[which(duplicated(eb_dt_location$id)),])
+
+# check for missing citation IDs
+
+# takes the ID numbers from the table, compares to a seq of all numbers b/w min and max
+# this works here because the seq starts from one - elsewhere, if need be, generate a seq from the first desired number to the last manually and compare to seq from data source
+idchk_seq <- eb_dt_location$id
+idchk_seq2 <- min(eb_dt_location$id):max(eb_dt_location$id)
+idchk_seq2[!idchk_seq2 %in% idchk_seq]
+
+# missing are OK for eb_citations_data_v2, location not applicable for missing IDs
+
+# long form year of study ----
 eb_dt_year <- eb_basedt %>% 
   select(citation,...2024:id) %>% 
   pivot_longer(cols = ...2024:...1960,
@@ -47,12 +60,20 @@ eb_dt_year <- eb_basedt %>%
 
 # fixing data formats
 eb_dt_year$year <- str_remove(eb_dt_year$year,"...")
-eb_dt_year$year <- as.numeric(eb_dt_year$year)
+eb_dt_year$year <- format(as.Date(eb_dt_year$year,
+                                  format = "%Y"),
+                          "%Y") ; View(eb_dt_year)
 
 # id 17 is a pre-print, assigned the year 2024 given its year of upload
 
+# check any missing citation ID 
+idchk_seq <- eb_dt_year$id
+idchk_seq2 <- min(eb_dt_year$id):max(eb_dt_year$id)
+idchk_seq2[!idchk_seq2 %in% idchk_seq]
 
-# join tables- id column is key ----
+# desired output of above chunk is integer[0]; If any are missing, those IDs will show instead. Year should not be missing for citations
+
+# join tables - id column is key ----
 
 ebd_join_catloc <- full_join(eb_dt_cat,eb_dt_location,"id")
 ebd_join_catloc <- ebd_join_catloc %>% select(id,citation.x,category,location)
@@ -61,24 +82,72 @@ ebd_join_complete <- left_join(ebd_join_catloc,eb_dt_year,"id")
 
 # check if both citations columns are identical
 identical(ebd_join_complete$citation.x,ebd_join_complete$citation)
+# intended output is TRUE
 
+# minor correction
 ebd_join_complete$category <- str_replace_all(ebd_join_complete$category,
                                               "Disease.Ecology..and.Health.",
                                               "Disease.Ecology.and.Health")
 
 # clean up into a df - ebd stands for ecology biology data
-ebd <- ebd_join_complete %>% select(id,category,location,year,citation)
+ebd <- ebd_join_complete %>%
+  select(id,category,location,year,citation) %>% 
+  arrange(year)
+
+View(ebd)
+
+# fix formatting of year column
+ebd$year <- as.Date(ebd$year,
+                    format = "%Y")
+ebd$year <- year(ebd$year)
+
+str(ebd)
+
+# optionally turn into factor in case of issues of ordering of cumulative numbers
+# ebd$year <- as.factor(ebd$year)
+
+# number of citations by category
+ebd %>% 
+  select(id,category) %>% 
+  unique() %>% 
+  group_by(category) %>% 
+  mutate(count = n()) %>% 
+  select(-id) %>% 
+  unique() %>% 
+  View()
 
 # Plotting ----
 
-# Line graph - citations by year and category
+# Cumulative number of citations by categories
 
+# data formatting 
 pub_years <- ebd %>% 
   select(-location,-citation) %>% 
-  unique() %>% 
-  group_by(year) %>% 
-  mutate(pub_count = n())
+  unique() %>%
+  group_by(year,category) %>%
+  mutate(pub_count = n()) %>% 
+  select(-id) %>% 
+  unique()
 
-ggplot(pub_years) +
-  geom_line(aes(year,pub_count,colour = category))
-  
+View(pub_years)
+
+pub_cumsum <- pub_years %>% 
+  ungroup() %>% 
+  group_by(category) %>% 
+  mutate(pub_cumsum = cumsum(pub_count))
+
+View(pub_cumsum)
+
+# check pub_cumsum endpoint totals match that of category wise citations totals, code available above 
+
+
+# Variable declarations for tweaks ----
+lwd = 1
+
+# plot 
+ggplot(pub_cumsum,aes(year,pub_cumsum, group = category, colour = category)) +
+  geom_line(linewidth = lwd)+
+  labs(x = "Year of Publication",
+       y = "Cumulative number of publications")+
+  theme_classic()
+
